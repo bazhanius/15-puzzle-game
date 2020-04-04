78eb282fae0647c17a6414150efd1b13a86b8707
bazhanius <a.bazhanov@gmail.com>
2020-04-04 23:15:09 +0300

    Possible fix for viewport (part II).

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,13 +9,6 @@ let gameState;
 
 function ready() {
 
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        let vw = window.innerWidth * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-        document.documentElement.style.setProperty('--vw', `${vw}px`);
-    });
-
 
     let btnExit = document.querySelector('#exit');
     let btnStart = document.querySelector('#start');
